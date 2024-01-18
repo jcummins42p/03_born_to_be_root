ccaebcb2c85dd2b4ab94e1ebeba839e75c17d93d
jcummins <jcummins42prague@gmail.com>
2024-01-18 14:56:28 +0000

    Finished monitoring, needs Network adding from 42 Project folder

--- monitoring.sh
+++ monitoring.sh
@@ -1,7 +1,21 @@
 #! /bin/bash
 
-wall (
-#Architecture:
+disku=$(df -m | awk '/^\/dev/ && !/\/boot\// { tdu += $3 } END { print tdu }')
+diskt=$(df -m | awk '/^\/dev/ && !/\/boot\// { tdu += $2 } END { print tdu }')
+diskp=$(awk "BEGIN {printf \"%.2f\", ($disku / $diskt) * 100}")
 
+echo "
+#Architecture	: $(uname -a)
+#CPU physical	: $(awk ' /cores/ {print $NF} ' /proc/cpuinfo | uniq)
+#vCPU		: $(awk ' /siblings/ {print $NF} ' /proc/cpuinfo | uniq)
+#Memory Usage	: $(free -m | awk ' /Mem:/ {printf "%d/%dMB (%.2f%%)", $3, $2, $3/$2*100}')
+#Disk Usage	: ${disku}/${diskt}MB (${diskp}%)
+#CPU usage	: $(top -bn1 | awk '/Cpu\(s\):/ {print $2+$4+$6 "%"}')
+#Last Boot	: $(who -b | awk '{print $(NF-1), $(NF)}')
+#LVM Active	: $(lsblk | awk '{tlvm="no"; if ($3=="lvm") tlvm="yes";} END {print tlvm}')
+#TCP Connections: $(ss -neopt state ESTABLISHED | wc -l) ESTABLISHED
+#User log	: $(users | wc -l)
+#Network	:
+#Sudo		: $(journalctl | awk /COMMAND/ | wc -l)
+"
 
-)
